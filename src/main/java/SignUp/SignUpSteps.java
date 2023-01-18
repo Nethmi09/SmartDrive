ec6e8af8c186e72f232b37b17b7f21831f94f780
nethmi12 <rdnethmiudara@gmail.com>
2023-01-18 16:36:16 +0530

    Automation 0.2

--- src/main/java/SignUp/SignUpSteps.java
+++ src/main/java/SignUp/SignUpSteps.java
@@ -29,12 +29,39 @@ public class SignUpSteps extends OpenWeb {
     public static void selectTitleDropdown(){
         try {
             Thread.sleep(7000);
-            Select se = new Select (driver.findElement(By.xpath("//select[@class='ng-dirty ng-valid ng-touched']")));
-            //Select title = new Select(driver.findElement(By.xpath("//select[@class='ng-dirty ng-valid ng-touched']")));
-// Select the option by index
-            se.selectByValue("Mr.");
-//            title.selectByVisibleText("Mr.");
-//            title.selectByIndex(1);
+            Select se = new Select (driver.findElement(By.xpath("//select[@formcontrolname='title']")));
+            se.selectByValue("Miss.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void enterFirstName(){
+        try {
+            driver.findElement(By.xpath("(//input[@formcontrolname='first_name'])")).clear();
+            driver.findElement(By.xpath("(//input[@formcontrolname='first_name'])")).sendKeys("Nethmi");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void enterLastName(){
+        try {
+            driver.findElement(By.xpath("(//input[@formcontrolname='last_name'])")).clear();
+            driver.findElement(By.xpath("(//input[@formcontrolname='last_name'])")).sendKeys("Udara");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void selectAddressDropdown(){
+        try {
+            driver.findElement(By.xpath("//input[@id='place']")).clear();
+            driver.findElement(By.xpath("//input[@id='place']")).sendKeys("Aareschlucht, Innertkirchen, Switzerland");
+
+//            Select ad = new Select (driver.findElement(By.xpath("//input[@id='place']")));
+//            ad.selectByValue("Aareschlucht, Innertkirchen, Switzerland");
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/SignUp/Tc_SignUpAsAStudentWithValidCredentials.java
+++ src/main/java/SignUp/Tc_SignUpAsAStudentWithValidCredentials.java
@@ -6,5 +6,8 @@ public class Tc_SignUpAsAStudentWithValidCredentials extends SignUpSteps{
        clickSignUpButton();
        clickStudentCreateAccountButton();
        selectTitleDropdown();
+        enterFirstName();
+        enterLastName();
+       selectAddressDropdown();
     }
 }
